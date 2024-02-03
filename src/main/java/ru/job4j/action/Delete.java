e926500d840ddf41fad29472fd668dad9f02e5f0
origin <git@github.com:OljaKn/job4j_design.git>
2024-02-03 14:21:30 +0300

    Sql tracker

--- src/main/java/ru/job4j/action/Delete.java
+++ src/main/java/ru/job4j/action/Delete.java
@@ -19,7 +19,9 @@ public class Delete implements UserAction {
         out.println("=== Удаление заявки ===");
         int id = input.askInt("Введите id: ");
         Item item = store.findById(id);
-        if (item != null) {
+        store.delete(id);
+        out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
+        /*if (item != null) {
             store.delete(id);
         } else {
             out.println("Ошибка удаления заявки.");
@@ -29,7 +31,7 @@ public class Delete implements UserAction {
             out.println("Заявка удалена успешно.");
         } else {
             out.println("Ошибка удаления заявки.");
-        }
+        }*/
         return true;
     }
 }
