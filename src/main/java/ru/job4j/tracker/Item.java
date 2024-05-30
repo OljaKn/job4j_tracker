472f7c20b3ed17c538db7648582e7ab347c9811a
origin <git@github.com:OljaKn/job4j_design.git>
2024-05-30 21:27:13 +0300

    1. ToMany [#301848]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,13 +1,21 @@
 package ru.job4j.tracker;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.job4j.toone.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "items")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
@@ -17,8 +25,13 @@ public class Item {
     private String name;
     private LocalDateTime created = LocalDateTime.now().withNano(0);
 
-    public Item() {
-    }
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "item_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
 
     public Item(String name) {
         this.name = name;
